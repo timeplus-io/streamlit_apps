f28179d8e558fa96f2d0ac68284f60febbb20f41
Jove Zhong <jove@timeplus.io>
2022-07-15 13:02:44 -0700

    rename files and change url

--- pages/101_🐦_live_tweets.py
+++ pages/101_🐦_live_tweets.py
@@ -13,7 +13,7 @@ with col_img:
 with col_txt:
     st.title("")
 with col_link:
-    st.markdown("[Source Code](https://github.com/timeplus-io/streamlit_apps/blob/main/live_tweets.py)", unsafe_allow_html=True)
+    st.markdown("[Source Code](https://github.com/timeplus-io/streamlit_apps/blob/main/pages/101_%F0%9F%90%A6_live_tweets.py)", unsafe_allow_html=True)
     
 env = (
     Env().schema(st.secrets["TIMEPLUS_SCHEMA"]).host(st.secrets["TIMEPLUS_HOST"]).port(st.secrets["TIMEPLUS_PORT"]).api_key(st.secrets["TIMEPLUS_API_KEY"])
